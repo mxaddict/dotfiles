1ed6f186ea5ea412de495d53a377898805dcb18a
mxaddict <mxaddict@kryptic.sh>
2025-09-15 10:22:08 +0800

    Programmer: A machine that turns coffee into code. - Anon

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -3,6 +3,7 @@ from qutebrowser.config.configfiles import ConfigAPI  # noqa: F401
 from qutebrowser.config.config import ConfigContainer  # noqa: F401
 config: ConfigAPI = config  # noqa: F821 pylint: disable=E0602,C0103
 c: ConfigContainer = c  # noqa: F821 pylint: disable=E0602,C0103
+config.source('./tokyonight.py')
 
 user = os.getlogin()
 
