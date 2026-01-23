4edf107dad407cb74856720966dca94c4cfa338d
mxaddict <mxaddict@kryptic.sh>
2026-01-23 15:40:01 +0800

    I'm not a visionary. I'm an engineer. - Linus Torvalds

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -4,9 +4,13 @@ return {
     opts = function(_, opts)
       opts.formatters_by_ft = opts.formatters_by_ft or {}
       opts.formatters_by_ft.gdscript = { "gdformat" }
+      opts.formatters_by_ft.sql = { "sqlfluff" }
 
       opts.formatters = opts.formatters or {}
       opts.formatters.gdformat = {}
+      opts.formatters.sqlfluff = {
+        args = { "fix", "-" },
+      }
     end,
   },
 }
