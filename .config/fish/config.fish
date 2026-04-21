8c0f8d054e2f8de41d8d6ae6b8a07fadc9b031aa
mxaddict <mxaddict@kryptic.sh>
2026-04-21 18:58:46 +0800

    The Linux kernel is a masterpiece of open source collaboration and a testament to the power of community-driven development. - Linus Torvalds

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -33,7 +33,7 @@ set -gx MAKEFLAGS "-j$JOBS"
 
 # Set paru pager
 set -gx PARU_PAGER "bat --color=always"
-set -gx MANPAGER "sh -c 'sed -u -e \"s/\\x1B\[[0-9;]*m//g; s/.\\x08//g\" | bat -p -lman'"
+set -gx MANPAGER "bat -p -lman"
 
 # FZF theme
 set -gx FZF_CTRL_T_OPTS "--preview 'bat -n --color=always {}'"
